3de25455fc98a33c05dc60a6d9a835649f98e1c8
tabrez05 <ahamedsmd007@gmail.com>
2025-01-12 10:00:00 -0800

    Implement file upload functionality

---
+++ upload.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request
+import os
+
+upload = Blueprint('upload', __name__)
+
+@upload.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join('uploads', filename))
+        return 'File uploaded successfully'
